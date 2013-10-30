7687502cf7bbefdfffa2f3c5d9bce34a68a188a2
Dan Sowter <dan@netengine.com.au>
2013-10-30 19:15:46 +1000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cinder::Application.config.secret_key_base = 'f7d1720b41c056224373559674f93d92d1f23b7fa5431760012b4a07b28601a9ceed0d0c9ac2a48acf88a7188cd8cedcd88a3b2ee83ea7b5c6170e98a5730f28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cinder::Application.config.session_store :cookie_store, key: '_cinder_session'
